9604cd88b087745f8c0cff0c9c8325da11712365
sceptile254 <asaumi1106n@gmail.com>
2022-06-15 13:33:15 +0000

    deviseを用いたmodel(user)を作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# ここはどこ？ => config/routes.rb
 Rails.application.routes.draw do
+  devise_for :users #deviseを使う際にURLにusersを含みます。
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
